0f345268c6af3041924da25b1d8fcaba9c7472f9
Dustin Brown <dustin.brown.k@gmail.com>
2022-09-04 18:25:11 -0400

    Added save test.

--- RESTDemo/src/test/java/dev/dustinb/RESTDemo/repository/RecipeRepositoryTest.java
+++ RESTDemo/src/test/java/dev/dustinb/RESTDemo/repository/RecipeRepositoryTest.java
@@ -1,9 +1,14 @@
 package dev.dustinb.RESTDemo.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
+import dev.dustinb.RESTDemo.recipe.Recipe;
+import dev.dustinb.RESTDemo.recipe.RecipeDetails;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 @SpringBootTest
@@ -16,14 +21,9 @@ public class RecipeRepositoryTest {
     @Autowired
     private RecipeRepository recipeRepository;
 
-    private TestInfo testInfo;
-    private TestReporter testReporter;
-
     @BeforeEach
     void init(TestInfo testInfo, TestReporter testReporter){
         System.out.println("Starting");
-        this.testInfo = testInfo;
-        this.testReporter = testReporter;
         testReporter.publishEntry("Running " + testInfo.getDisplayName() + " with tags " + testInfo.getTags());
     }
 
@@ -47,13 +47,31 @@ public class RecipeRepositoryTest {
             @Test
             @DisplayName("test number of steps for 'chai'.")
             void hasCorrectNumSteps(){
-                final int chaiSteps = 4;
+                final int chaiSteps = 4; //num from recipe.json
                 assertEquals(chaiSteps, recipeRepository.recipeDetails("chai").get("details").getnumSteps());
+            }
+
+            @Test
+            @DisplayName("test ingredient list values for 'chai'.")
+            void hasCorrectIngredients(){
+                final String ingredient = "100ml milk"; //from recipe.json
+                Map<String, RecipeDetails> chaiDetails = recipeRepository.recipeDetails("chai");
+                assertTrue(chaiDetails.get("details").getIngredients().get(1).equalsIgnoreCase(ingredient));
             }
         }
 
     @Test
     void saveRecipe() {
+        List<String> instructions = new ArrayList<>();
+        List<String> ingredients = new ArrayList<>();
+        instructions.add("Freeze hotdogs");
+        instructions.add("Boil ice cream floats");
+        instructions.add("Mix and re-freeze");
+        ingredients.add("1 hotdog");
+        ingredients.add("3 Ice cream floats");
+        Recipe newRecipe = new Recipe("Hotdog Ice Cream", ingredients, instructions);
+        recipeRepository.saveRecipe(newRecipe);
+        assertTrue(recipeRepository.findAll().get("recipeNames").contains("Hotdog Ice Cream"));
     }
 
     @Test
